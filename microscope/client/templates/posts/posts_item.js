1d47620e002c06b32a82b971599a094e114d0353
nitya <n.narasimhan@gmail.com>
2015-02-06 14:08:16 -0500

    4-1 Added a posts collection

--- microscope/client/templates/posts/posts_item.js
+++ microscope/client/templates/posts/posts_item.js
@@ -3,7 +3,8 @@
 // list template handler, by providing the relevant 'domain' context
 Template.postItem.helpers({ 
 	domain: function() {
-		var a = document.createElement('a'); a.href = this.url;
+		var a = document.createElement('a'); 
+		a.href = this.url;
 		return a.hostname;
 	} 
 });
---
+++ microscope/lib/collections/posts.js
@@ -0,0 +1,2 @@
+// 4.1 Adding collections (but in insecure, autopublish mode)
+Posts = new Mongo.Collection('posts');
