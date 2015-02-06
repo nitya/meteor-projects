8bccdcbf9cd77671be9e33a47b8846572e96c157
nitya <n.narasimhan@gmail.com>
2015-02-06 15:38:37 -0500

    5.3 Routing to a single post page.

--- microscope/lib/router.js
+++ microscope/lib/router.js
@@ -29,4 +29,29 @@ Router.configure({
 // building or referencing dynamic links later using 'pathFor'
 // 		e.g., href="{{pathFor 'postsList'}}" 
 // will now dynamically bind to "/" given the route declaration below
-Router.route('/', {name: 'postsList'});
+Router.route('/', 
+	{ name: 'postsList'}
+);
+
+// 5.3 the "_id" parameter is now available in the 'params' array
+// associated with the router. Also note that 'data' is a reserved
+// word that effectively sets the data context for the associated 
+// template rendered against this route.
+// i.e., now postPage template can use "this" to refer to object
+//   passed in via the data property here
+// 
+// (Aside) 
+// data contexts can be passed to a template helper in many ways. 
+//		1. Routers use the 'data' property. 
+//		2. The {{#each}} template iterator sets it to current item
+//      3. The {{#with}} syntax enables it to be set explicitly for a 
+//			 contained template e.g., 
+//				{{#with myPostData}} {{> postPage}} {{/with}}
+//      4. Or just pass it directly as a paramter to template call e.g.,
+//				{{> postPage myPostData}}
+//
+Router.route('/posts/:_id',{ 
+		name: 'postPage',
+		data: function() { return Posts.findOne(this.params._id); }
+	}
+);
