73aea97b72295d911c7b89306f79f6b3e80dbdae
nitya <n.narasimhan@gmail.com>
2015-02-06 21:57:46 -0500

    7.8 Sort posts by submitted timestamp.

--- microscope/client/templates/posts/posts_list.js
+++ microscope/client/templates/posts/posts_list.js
@@ -4,8 +4,10 @@
 // 4.3 replaced the static data with a dyanmic database query
 //     note that find() returns a cursor = reactive data source
 //      if you want to get static data, add .fetch()
+//
+// 7.8 add sort parameters to find, to get sorted results
 Template.postsList.helpers({
   posts: function(){
-    return Posts.find();
+    return Posts.find({}, {sort: {submitted: -1}});
   }
 });
