43b4bd8037b657deeac525ea5cbecff575124a2f
nitya <n.narasimhan@gmail.com>
2015-02-06 21:51:26 -0500

    7.6 Use a method to submit the post.

--- microscope/client/templates/posts/post_submit.js
+++ microscope/client/templates/posts/post_submit.js
@@ -9,9 +9,21 @@ Template.postSubmit.events({
 			url: $(e.target).find('[name=url]').val(), 
 			title: $(e.target).find('[name=title]').val()
 		};
-		post._id = Posts.insert(post);
-		
+
+		// 7.6 Replace the simple Posts.insert with a Method
+		// call if the insert requires additional processing
+		// Methods are server-side functions called from client
+		// (callbacks return error or success status)
+		Meteor.call('postInsert', post, function(error, result){
+			if (error)	// display error to the user and abort
+				return alert(error.reason);
+      		Router.go('postPage', {_id: result._id});
+      	});
+
+		/* -- replaced in 7.6 above
 		// Once inserted, then route to details page
+		post._id = Posts.insert(post);
 		Router.go('postPage', post);
+		*/
 	}
 });
--- microscope/lib/collections/posts.js
+++ microscope/lib/collections/posts.js
@@ -9,3 +9,31 @@ Posts.allow({
 		return !! userId;
 	}
 });
+
+// 7.6 Declare postInsert method
+// Note that we are using check methods to validate data
+Meteor.methods({
+	postInsert: function(postAttributes){
+
+		// validate data
+		check(Meteor.userId(), 
+			String
+		);
+		check(postAttributes, {
+			title:String, 
+			url:String
+		});
+
+		// extend input attributes with user info
+		var post = _.extend(postAttributes, {
+			userId: user._id,
+			author: user.username,
+			submitted: new Date()
+		});
+
+		// insert object into collection on server
+		var postId = Posts.insert(post);
+		return {_id: postId};
+
+	};
+});
