06ea3bc5a005fbfaf8cbd283ad7b0a902cd21ea5
nitya <n.narasimhan@gmail.com>
2015-02-06 21:02:39 -0500

    7.2 Removed insecure, and allowed certain writes to posts.

--- microscope/lib/collections/posts.js
+++ microscope/lib/collections/posts.js
@@ -1,2 +1,10 @@
 // 4.1 Adding collections (but in insecure, autopublish mode)
 Posts = new Mongo.Collection('posts');
+
+// 7.2 Remove 'insecure', add explict security rules
+Posts.allow({
+	insert: function(userId, doc){
+		// only allow posting if you are logged in
+		return !! userId;
+	}
+});
