35abd256f7f5c0ba170cfae8adecb036e126cd64
nitya <n.narasimhan@gmail.com>
2015-02-11 12:40:06 -0500

    2. Added basic bootstrap, account login

---
+++ leaflet-parties/client/templates/page.js
@@ -0,0 +1,8 @@
+// Declare page template data helpers here
+//
+// 2. Bind page title
+//
+Template.page.helpers({
+	brandLink: "All Tomorrow's Parties",
+	instruction: "Double click the map to post a party!"
+});
